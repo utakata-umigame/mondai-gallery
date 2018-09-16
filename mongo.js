16d0a7b41375683b1ea0f3c3378e36a9e0d6d1f4
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-09-16 22:58:06 +0900

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -5,7 +5,7 @@ const assert = require("assert");
 //Connect to MongoDB
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
-const url = process.env.MONGODB_HOST || "mongodb://localhost:27017";
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
 let db = {};
 MongoClient.connect(url, function(err, client) {
   db.User = client.db("gallery").collection("user");
