55c0873f48beccf368f681dc972aa2bbd1d0edd9
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-09-16 23:40:32 +0900

    Set DB name

--- mongo.js
+++ mongo.js
@@ -8,9 +8,9 @@ const ObjectId = require("mongodb").ObjectId;
 const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
 let db = {};
 MongoClient.connect(url, function(err, client) {
-  db.User = client.db("gallery").collection("user");
-  db.MyList = client.db("gallery").collection("myList");
-  db.Counter = client.db("gallery").collection("counter");
+  db.User = client.db(process.env.MONGODB_NAME||"gallery").collection("user");
+  db.MyList = client.db(process.env.MONGODB_NAME||"gallery").collection("myList");
+  db.Counter = client.db(process.env.MONGODB_NAME||"gallery").collection("counter");
   db.Counter.insertOne({id: "list_id", seq: 1});
   console.log("connected");
 });
