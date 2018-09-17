485f8fb58478c204a43566c03946b7036dea4c16
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-09-17 22:31:49 +0900

    Remove unnecessary labels

--- client/src/components/LogIn.vue
+++ client/src/components/LogIn.vue
@@ -2,9 +2,9 @@
   <div>
     <h2>ログイン</h2>
     <form :class="input-group" method="post" class="mb-2">
-      <label>ユーザー名(英数、必須)</label>
+      <label>ユーザー名</label>
       <input class="form-control mb-2" type="text" name="username" placeholder='User Name' required="required">
-      <label>パスワード(英数、必須)</label>
+      <label>パスワード</label>
       <input class="form-control mb-2" type="password" name="password" placeholder='Password' required="required">
       <input class="btn btn-primary mb-2" type="submit" value='ログイン'>
     </form>
