bade91d2aad706be67443e4f259c88a10d7427b1
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-09-23 14:30:11 +0000

    Add files

---
+++ client/src/components/ProfileDialog.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="form">
+    <b-form-textarea
+      v-model="bio"
+      placeholder="自己紹介"
+      :rows="6">
+    </b-form-textarea>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    bio: String
+  }
+}
+</script>>
