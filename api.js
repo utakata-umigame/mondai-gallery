390858fd3257d1cf48f6542b14f5bc40465aba13
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-09-28 17:47:44 +0900

    Separate apis' routes and their implementation

---
+++ api.js
@@ -0,0 +1,183 @@
+'use strict';
+
+const crypto = require('crypto');
+const moment = require('moment');
+const db = require('./mongo');
+
+module.exports = {
+  root: (req, res) => {
+    res.json({
+      "name": "test",
+      "editor": "test",
+      "mondai":[
+        {"id":1, "site": "cindy", "title": "", "author": "", "description": "", "genre": "umigame"}
+      ]
+    });  
+  },
+  profileFromID: (req, res) => {
+    db.User.findOne({id: parseInt(req.params.id)}, (err, doc) => {
+      if (doc) {
+        res.json({
+          "id": doc.id,
+          "nickname": doc.nickname,
+          "bio": doc.bio,
+          "latethink": doc.latethink,
+          "cindy": doc.cindy,
+          "R": doc.R,
+          "twitter": doc.twitter,
+          "github": doc.github,
+          "signup_date": doc.signup_date
+        });
+        return;
+      }
+      res.json({'error':'error'});
+    });
+  },
+  allList: (req, res) => {
+    db.MondaiList.find({}).toArray((error, docs) => {
+        if (docs) {
+          res.json(docs.map(function (x) {
+            return {
+              id: x.id,
+              name: x.name,
+              fromMyMondais: x.fromMyMondais,
+              editor: x.editor,
+              description: x.description,
+              updateDate: x.updateDate
+            };
+          }));
+        }
+        else {
+          res.json({'error': 'error'});
+        }
+    });
+  },
+  listFromID: (req, res) => {
+   db.MondaiList.findOne({id: parseInt(req.params.id)}, (err, doc) => {
+     if (doc) {
+       res.json(doc);
+       return;
+     }
+     if (err) {
+       console.log(err);
+     }
+     res.json({'error': 'error'});
+   });
+  },
+  login: (req, res) => {
+    db.User.findOne({username: req.body.username}, (err, doc) => {
+      if (doc) res.json({'message': 'Logged in', 'user': doc});
+      else res.json({'error':'error'});
+    });
+  },
+  signUp: (req, res) => {
+    if (req.body['username'] === '' || req.body['password'] === '') {
+      res.json({'error': 'Credentials are missing'});
+      return;
+    }
+    //ハッシュ値を計算して照合
+    let shasum = crypto.pbkdf2Sync(req.body['password'], process.env.SALT||'yoursalthere', 10000, 64, 'sha512');
+    let hash = shasum.toString('hex'); 
+    let data = {
+      nickname: req.body['nickname'],
+      username: req.body['username'],
+      password: hash,
+      signup_date: moment()
+        .utcOffset('+09:00')
+        .format('YYYY/MM/DD HH:mm:ss')
+    };
+    if (!data.username.match(/[^A-Za-z0-9]+/)) {
+      db.User.findOne({username: data.username}, function(err, doc) {
+        if (err) {
+          console.log(err);
+        }
+        if (doc) {
+          res.json({'error': 'Duplicated username'});
+        } else {
+          db.Counter.findOneAndUpdate({id:"user_id"},{$inc: {seq: 1}}, {new: true}, (err, res) => {
+            console.log(res.value);
+            let id = res.value.seq||1;
+            data.id = id;
+            db.User.insertOne(data, (err, result) => {});
+          })
+          res.json({"message": 'success'});
+        }
+      });
+    } else {
+      res.json({'error': 'Invalid username'});
+    }
+  },
+  /* Functions below require authentication */
+  user: (req, res) => {
+    if (req.user)
+      res.json(req.user);
+    else res.status(403).send({
+      "success": "false",
+      "message": "Not authenticated"
+    });
+  },
+  myPage: (req, res) => {
+    if (!req.user) {
+      res.json({'error': 'error1'});
+      return;
+    }
+    db.User.findOne({username: req.user.username}, (err, doc) => {
+      if (doc) {
+        if (doc.username) {
+          res.json({
+            "id": doc.id,
+            "username": doc.username,
+            "nickname": doc.nickname,
+            "bio": doc.bio,
+            "latethink": doc.latethink,
+            "cindy": doc.cindy,
+            "R": doc.R,
+            "twitter": doc.twitter,
+            "github": doc.github,
+            "signup_date": doc.signup_date
+          });
+          return;
+        }
+      }
+      res.json({'error':'error'});
+    });
+  },
+  editProfile: (req, res) => {
+    let obj = req.body;
+    console.log(obj);
+    db.User.updateOne({"username": req.user.username}, {$set: {"bio": obj.bio, "latethink": obj.latethink, "cindy": obj.cindy, "R": obj.R, "twitter": obj.twitter, "github": obj.github}}, (err, doc) => {})
+    res.json({"message": "success"})
+  },
+  editList: (req, res) => {
+    let obj = req.body
+    let updateDate = moment()
+      .utcOffset('+09:00')
+      .format('YYYY/MM/DD HH:mm')
+    db.MondaiList.updateOne({"id": req.body.id, "editor.username": req.user.username}, {$set: {"name": obj.name, editor: req.user,"fromMyMondais": obj.fromMyMondais, "description": obj.description, "mondai": obj.mondai, "updateDate": updateDate}}, (err, doc) => {
+       if(err) res.status(403).send({
+        'success': 'false',
+        'message': 'Cannot edit'
+       });
+       else {
+        res.json({"message": "success"});
+       }
+    });
+  },
+  addList: (req, res) => {
+    db.Counter.findOneAndUpdate({id:"list_id"},{$inc: {seq: 1}}, {new: true}, (err, res) => {
+      console.log(res.value);
+      let id = res.value.seq||1;
+      req.body.id = id;
+      req.body.editor = req.user;
+      req.body.updateDate = moment()
+        .utcOffset('+09:00')
+        .format('YYYY/MM/DD HH:mm')
+      db.MondaiList.insertOne(req.body, (err, result) => {});
+    })
+    res.json({"message": "Success"});
+  },
+  logout: function (req, res) {
+    req.logout();
+    res.json({});
+  }
+};
--- route.js
+++ route.js
@@ -2,127 +2,21 @@
 
 const express = require('express');
 const crypto = require('crypto');
-var moment = require('moment');
+const moment = require('moment');
 const db = require('./mongo');
 const passport = require('./passport');
+const apis = require('./api');
 const router = express();
 
-let id = 1;
-router.get("/", (req, res) => {
-  let obj = {
-    "name": "test",
-    "editor": "test",
-    "mondai":[
-      {"id":1, "site": "cindy", "title": "", "author": "", "description": "", "genre": "umigame"}
-    ]
-  }
-  res.json(obj);
-});
-/* プロフィール */
-router.get("/profile/show/:id", (req, res) => {
-  db.User.findOne({id: parseInt(req.params.id)}, (err, doc) => {
-    if (doc) {
-      res.json({
-        "id": doc.id,
-        "nickname": doc.nickname,
-        "bio": doc.bio,
-        "latethink": doc.latethink,
-        "cindy": doc.cindy,
-        "R": doc.R,
-        "twitter": doc.twitter,
-        "github": doc.github,
-        "signup_date": doc.signup_date
-      });
-      return;
-    }
-    res.json({'error':'error'});
-  });
-});
-/* 問題リスト全部 */
-router.get("/mondaiList", (req, res) => {
-  db.MondaiList.find({}).toArray((error, docs) => {
-      if (docs) {
-        res.json(docs.map(function (x) {
-          return {
-            id: x.id,
-            name: x.name,
-            fromMyMondais: x.fromMyMondais,
-            editor: x.editor,
-            description: x.description,
-            updateDate: x.updateDate
-          };
-        }));
-      }
-      else {
-        res.json({'error': 'error'});
-      }
-  });
-});
-/* 問題リスト1つ */
-router.get("/mondaiList/show/:id", (req, res) => {
-   db.MondaiList.findOne({id: parseInt(req.params.id)}, (err, doc) => {
-     if (doc) {
-       res.json(doc);
-       return;
-     }
-     if (err) {
-       console.log(err);
-     }
-     res.json({'error': 'error'});
-     
-   });
-});
-/* ログイン */
+router.get("/", apis.root);
+router.get("/profile/show/:id", apis.profileFromID);
+router.get("/mondaiList", apis.allList);
+router.get("/mondaiList/show/:id", apis.listFromID);
+router.post("/signup", apis.signUp);
 router.post(
-  '/login',
+  "/login",
   passport.authenticate('local'), 
-  function(req, res) {
-    db.User.findOne({username: req.body.username}, (err, doc) => {
-    if (doc) res.json({'message': 'Logged in', 'user': doc});
-    else res.json({'error':'error'});
-  });
-});
-/* 登録 */
-router.post('/signup', function(req, res) {
-  console.log();
-  if (req.body['username'] === '' || req.body['password'] === '') {
-    res.json({'error': 'Credentials are missing'});
-    return;
-  }
-  //ハッシュ値を計算して照合
-  let shasum = crypto.pbkdf2Sync(req.body['password'], process.env.SALT||'yoursalthere', 10000, 64, 'sha512');
-  /* let shasum = crypto.createHash('sha1');
-  shasum.update(req.body['password']); */
-  let hash = shasum.toString('hex'); 
-  let data = {
-    nickname: req.body['nickname'],
-    username: req.body['username'],
-    password: hash,
-    signup_date: moment()
-      .utcOffset('+09:00')
-      .format('YYYY/MM/DD HH:mm:ss')
-  };
-  if (!data.username.match(/[^A-Za-z0-9]+/)) {
-    db.User.findOne({username: data.username}, function(err, doc) {
-      if (err) {
-        console.log(err);
-      }
-      if (doc) {
-        res.json({'error': 'Duplicated username'});
-      } else {
-        db.Counter.findOneAndUpdate({id:"user_id"},{$inc: {seq: 1}}, {new: true}, (err, res) => {
-          console.log(res.value);
-          let id = res.value.seq||1;
-          data.id = id;
-          db.User.insertOne(data, (err, result) => {});
-        })
-        res.json({"message": 'success'});
-      }
-    });
-  } else {
-    res.json({'error': 'Invalid username'});
-  }
-});
+  apis.login);
 function isAuthenticated (req, res, next) {
   // 認証チェック
   if (req.isAuthenticated()) {
@@ -131,6 +25,14 @@ function isAuthenticated (req, res, next) {
     res.json({"error": "Authentication failed"});
   }
 }
+router.get("/user", isAuthenticated, apis.user);
+router.get("/mypage", isAuthenticated, apis.myPage);
+router.get("/logout", isAuthenticated, apis.logout);
+router.post("/profile/edit", isAuthenticated, apis.editProfile);
+router.post("/mondaiList/edit/:id", isAuthenticated, apis.editList);
+router.post("/add", isAuthenticated, apis.addList);
+
+module.exports = router;
 /*
 var crypto = require('crypto');
 // jwt
@@ -177,82 +79,3 @@ router.use(function(req, res, next) {
     next();
   });
 });*/
-
-router.get("/user", isAuthenticated,(req, res) => {
-  if (req.user)
-    res.json(req.user);
-  else res.status(403).send({
-    "success": "false",
-    "message": "Not authenticated"
-  });
-});
-/* マイページ */
-router.get("/mypage", isAuthenticated,(req, res) => {
-  if (!req.user) {
-    res.json({'error': 'error1'});
-    return;
-  }
-  db.User.findOne({username: req.user.username}, (err, doc) => {
-    if (doc) {
-      if (doc.username) {
-        res.json({
-          "id": doc.id,
-          "username": doc.username,
-          "nickname": doc.nickname,
-          "bio": doc.bio,
-          "latethink": doc.latethink,
-          "cindy": doc.cindy,
-          "R": doc.R,
-          "twitter": doc.twitter,
-          "github": doc.github,
-          "signup_date": doc.signup_date
-        });
-        return;
-      }
-    }
-    res.json({'error':'error'});
-  });
-});
-router.post("/profile/edit", isAuthenticated,(req, res) => {
-  let obj = req.body;
-  console.log(obj);
-  db.User.updateOne({"username": req.user.username}, {$set: {"bio": obj.bio, "latethink": obj.latethink, "cindy": obj.cindy, "R": obj.R, "twitter": obj.twitter, "github": obj.github}}, (err, doc) => {})
-  res.json({"message": "success"})
-});
-/* リスト編集 */
-router.post("/mondaiList/edit/:id", isAuthenticated,(req, res) => {
-  let obj = req.body
-  let updateDate = moment()
-    .utcOffset('+09:00')
-    .format('YYYY/MM/DD HH:mm')
-  db.MondaiList.updateOne({"id": req.body.id, "editor.username": req.user.username}, {$set: {"name": obj.name, editor: req.user,"fromMyMondais": obj.fromMyMondais, "description": obj.description, "mondai": obj.mondai, "updateDate": updateDate}}, (err, doc) => {
-     if(err) res.status(403).send({
-      'success': 'false',
-      'message': 'Cannot edit'
-     });
-     else {
-      res.json({"message": "success"});
-     }
-  });
-});
-/* リスト追加 */
-router.post("/add", isAuthenticated,(req, res) => {
-  db.Counter.findOneAndUpdate({id:"list_id"},{$inc: {seq: 1}}, {new: true}, (err, res) => {
-    console.log(res.value);
-    let id = res.value.seq||1;
-    req.body.id = id;
-    req.body.editor = req.user;
-    req.body.updateDate = moment()
-      .utcOffset('+09:00')
-      .format('YYYY/MM/DD HH:mm')
-    db.MondaiList.insertOne(req.body, (err, result) => {});
-  })
-  res.json({"message": "Success"});
-});
-/* ログアウト */
-router.get('/logout', isAuthenticated, function (req, res) {
-  req.logout();
-  res.json({});
-});
-
-module.exports = router;
