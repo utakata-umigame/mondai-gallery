57a8d5e43cd97fd8485e9fa9226072b1ffcc47ed
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-11-10 12:02:45 +0000

    Update layout

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -37,7 +37,7 @@
             </div>
             <div class="card-text">
               <account-link :profile="profile"></account-link>
-              <p class="multiline">{{ profile.bio }}</p>
+              <p class="multiline mb">{{ profile.bio }}</p>
               <p>登録日時：{{profile.signup_date}}</p>
             </div>
           </div>
