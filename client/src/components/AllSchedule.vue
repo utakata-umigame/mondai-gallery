526b7a39dc703cf4cfede614df182b575d194cbd
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-11-12 12:10:02 +0900

    Update layout

--- client/src/components/AllSchedule.vue
+++ client/src/components/AllSchedule.vue
@@ -1,5 +1,20 @@
 <template>
-  <ScheduleView :schedule="schedule"/>
+  <div>
+    <div class="content">
+      <b-datepicker
+          inline
+          v-model="date"
+          :month-names="['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']"
+          :day-names="['日','月','火','水','木','金','土']"
+          :events="events()"
+          @input="filter"
+          indicators="dots"
+          >
+      </b-datepicker>
+      <button @click="reset" class="button is-primary is-outlined">リセット</button>
+  </div>
+    <ScheduleView :schedule="filtered"/>
+  </div>
 </template>
 <script>
 import ScheduleView from '@/components/ScheduleView'
@@ -9,8 +24,12 @@ export default {
   },
   data () {
     return {
+      date: new Date(),
       schedule: {
         color: '',
+        tasks: []
+      },
+      filtered: {
         tasks: []
       }
     }
@@ -24,9 +43,29 @@ export default {
           return t
         })
       })
+      .then(_ => {
+        this.filtered = Object.assign({}, this.schedule)
+      })
   },
   methods: {
-
+    events () {
+      return this.schedule.tasks.map(x => {
+        let d = x.endDate
+        return {
+          date: d,
+          type: 'is-info'
+        }
+      })
+    },
+    filter () {
+      this.filtered.tasks = this.schedule.tasks.filter(x => this.fullDate(x.endDate) === this.fullDate(this.date))
+    },
+    reset () {
+      this.filtered = Object.assign({}, this.schedule)
+    },
+    fullDate (date) {
+      return date.getYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate()
+    }
   }
 }
 </script>
