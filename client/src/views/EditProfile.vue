29d40a49a15d278bad6e1ca4228105471b1e2cee
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-11-16 15:16:30 +0900

    Update layout

--- client/src/views/EditProfile.vue
+++ client/src/views/EditProfile.vue
@@ -13,114 +13,123 @@
         <li class="is-active">
           <a href="#">
             <span class="icon is-small is-primary">
-              <b-icon icon="account-box"/>
+              <b-icon icon="settings"></b-icon>
             </span>
-            <span>プロフィールを編集</span>
+            <span>個人設定</span>
           </a>
         </li>
       </ul>
     </nav>
-    <b-tabs v-model="activeTab" position="is-centered" class="block" expanded>
-      <b-tab-item label="プロフィール情報">
-        <section class="section">
-        <p class="subtitle">
-          <img :src="profile.picUrl" v-if="profile.picUrl" width="48" height="48" alt="No Image">
-          <b-icon size="is-medium" icon="account-box" :style="{'color': profile.color}" v-else/>
-          {{profile.nickname}}
-        </p>
-        <b-field
-          label="プロフィール画像">
-          <b-input
-            type="text"
-            v-model="profile.picUrl"
-            placeholder="画像のURL">
-          </b-input>
-        </b-field>
-        <b-field
-          label="テーマカラー(カラーコード)">
-          <b-input
-            type="text"
-            v-model = "profile.color"
-            placeholder="例)#000">
-          </b-input>
-        </b-field>
-        <b-field>
-          <b-input
-            type="color"
-            v-model = "profile.color">
-          </b-input>
-        </b-field>
-        <b-field label="自己紹介">
-          <b-input
-            type="textarea"
-            v-model.trim="profile.bio"
-            placeholder="自己紹介"
-            :rows="6">
-          </b-input>
-        </b-field>
-      </section>
-      </b-tab-item>
-      <b-tab-item label="アカウント">
-        <section class="section">
-          <b-field
-            label="ラテシンのID">
-            <b-input
-              type="number"
-              v-model = "profile.latethink"
-              placeholder=".../mondai/profile/[ID]">
-            </b-input>
-          </b-field>
-          <b-field
-            label="CindyのID">
-            <b-input
-              type="number"
-              v-model = "profile.cindy"
-              placeholder=".../profile/show/[ID]">
-            </b-input>
-          </b-field>
-          <b-field
-            label="R鯖のID">
-            <b-input
-              type="number"
-              v-model = "profile.R"
-              placeholder=".../Mmail/userpage/[ID]">
-            </b-input>
-          </b-field>
-          <b-field
-            label="らてらて鯖のID">
-            <b-input
-              type="number"
-              v-model = "profile.latelate"
-              placeholder=".../mondai/profile/[ID]">
-            </b-input>
-          </b-field>
-          <b-field
-            label="らてしんPROのID">
-            <b-input
-              type="number"
-              v-model = "profile.latePro"
-              placeholder=".../users/profile/[ID]">
-            </b-input>
-          </b-field>
-          <b-field
-            label="TwitterのID">
-            <b-input
-              type="text"
-              v-model = "profile.twitter"
-              placeholder=".../twitter.com/[ID]">
-            </b-input>
-          </b-field>
-          <b-field
-            label="GitHubのID">
-            <b-input
-              type="text"
-              v-model = "profile.github"
-              placeholder=".../github.com/[ID]">
-            </b-input>
-          </b-field>
-        </section>
-      </b-tab-item>
-    </b-tabs>
+    <div class="columns">
+      <aside class="menu column is-one-quarter">
+        <p class="menu-label">Settings</p>
+        <ul class="menu-list">
+          <li><a @click="activeTab = 0">プロフィール</a></li>
+          <li><a @click="activeTab = 1">アカウント</a></li>
+        </ul>
+      </aside>
+      <div  class="column is-three-quarter">
+        <div label="プロフィール情報" v-if="activeTab === 0">
+            <section class="section">
+            <p class="subtitle">
+              <img :src="profile.picUrl" v-if="profile.picUrl" width="48" height="48" alt="No Image">
+              <b-icon size="is-medium" icon="account-box" :style="{'color': profile.color}" v-else/>
+              {{profile.nickname}}
+            </p>
+            <b-field
+              label="プロフィール画像">
+              <b-input
+                type="text"
+                v-model="profile.picUrl"
+                placeholder="画像のURL">
+              </b-input>
+            </b-field>
+            <b-field
+              label="テーマカラー(カラーコード)">
+              <b-input
+                type="text"
+                v-model = "profile.color"
+                placeholder="例)#000">
+              </b-input>
+            </b-field>
+            <b-field>
+              <b-input
+                type="color"
+                v-model = "profile.color">
+              </b-input>
+            </b-field>
+            <b-field label="自己紹介">
+              <b-input
+                type="textarea"
+                v-model.trim="profile.bio"
+                placeholder="自己紹介"
+                :rows="6">
+              </b-input>
+            </b-field>
+          </section>
+        </div>
+        <div label="アカウント" v-if="activeTab === 1">
+          <section class="section">
+            <b-field
+              label="ラテシンのID">
+              <b-input
+                type="number"
+                v-model = "profile.latethink"
+                placeholder=".../mondai/profile/[ID]">
+              </b-input>
+            </b-field>
+            <b-field
+              label="CindyのID">
+              <b-input
+                type="number"
+                v-model = "profile.cindy"
+                placeholder=".../profile/show/[ID]">
+              </b-input>
+            </b-field>
+            <b-field
+              label="R鯖のID">
+              <b-input
+                type="number"
+                v-model = "profile.R"
+                placeholder=".../Mmail/userpage/[ID]">
+              </b-input>
+            </b-field>
+            <b-field
+              label="らてらて鯖のID">
+              <b-input
+                type="number"
+                v-model = "profile.latelate"
+                placeholder=".../mondai/profile/[ID]">
+              </b-input>
+            </b-field>
+            <b-field
+              label="らてしんPROのID">
+              <b-input
+                type="number"
+                v-model = "profile.latePro"
+                placeholder=".../users/profile/[ID]">
+              </b-input>
+            </b-field>
+            <b-field
+              label="TwitterのID">
+              <b-input
+                type="text"
+                v-model = "profile.twitter"
+                placeholder=".../twitter.com/[ID]">
+              </b-input>
+            </b-field>
+            <b-field
+              label="GitHubのID">
+              <b-input
+                type="text"
+                v-model = "profile.github"
+                placeholder=".../github.com/[ID]">
+              </b-input>
+            </b-field>
+          </section>
+        </div>
+      </div>
+    </div>
     <div class="buttons has-addons">
       <span class="button is-primary is-outlined" @click="submit">保存</span>
       <span class="button is-danger is-outlined" @click="$router.push('/mypage')">キャンセル</router-link></span>
