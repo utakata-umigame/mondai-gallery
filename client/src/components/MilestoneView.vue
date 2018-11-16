52fe62502907efb1122b5bf0c2756254b58a5625
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-11-16 14:10:12 +0900

    Update MilestoneView.vue

--- client/src/components/MilestoneView.vue
+++ client/src/components/MilestoneView.vue
@@ -24,13 +24,20 @@ export default {
           description: x.description
         }
       })
-      return [{
-        year: {
-          from: Math.min(...years),
-          to: Math.max(...years)
-        },
-        items: this.sort(data)
-      }]
+      let sorted = this.sort(data)
+      let set = new Set(years)
+      let res = []
+      set.forEach(y => {
+        console.log(y)
+        res.push({
+          year: {
+            from: y,
+            to: '　'
+          },
+          items: sorted.filter(x => x.date.getFullYear() === y)
+        })
+      })
+      return res
     },
     day: () => ({
       0: '日',
@@ -40,6 +47,20 @@ export default {
       4: '木',
       5: '金',
       6: '土',
+    }),
+    month: () => ({
+      0: 'January',
+      1: 'February',
+      2: 'March',
+      3: 'April',
+      4: 'May',
+      5: 'June',
+      6: 'July',
+      7: 'August',
+      8: 'September',
+      9: 'October',
+      10: 'November',
+      11: 'December'
     })
   },
   methods: {
@@ -49,57 +70,14 @@ export default {
       })
     },
     formatDate(date) {
-      let year = date.getFullYear()
       let month = date.getMonth() + 1
       let dateNum = date.getDate()
       let dayNum = date.getDay()
-      return year + '年' + month + '月' + dateNum + '日' + '(' + this.day[dayNum] + ')'
+      return month + '/' + dateNum + '(' + this.day[dayNum] + ')'
     }
   },
   data () {
     return {
-      /* timelineItems: [
-        {
-          year: {
-            from: '2017',
-            to: '2018'
-          },
-          items: [
-            {
-              nameMonth: 'February　3',
-              title: 'Five',
-              description:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.'
-            },
-            {
-              nameMonth: 'January',
-              title: 'Five',
-              description:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.'
-            }
-          ]
-        },
-        {
-          year: {
-            from: '2016',
-            to: '2017'
-          },
-          items: [
-            {
-              nameMonth: 'February',
-              title: 'Five',
-              description:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.'
-            },
-            {
-              nameMonth: 'January',
-              title: 'Five',
-              description:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.'
-            }
-          ]
-        },
-      ] */
     }
   }
 }
