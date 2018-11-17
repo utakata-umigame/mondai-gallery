50c90338df164ea1eaa552409cd5792ba95baabe
otn-carrot <43202236+otn-carrot@users.noreply.github.com>
2018-11-18 00:00:19 +0900

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    tumblr: {
+    tumblrToken: {
       token: '',
       secret: ''
     },
@@ -57,7 +57,7 @@ export default new Vuex.Store({
       state.savedProfiles[value.id] = value
     },
     setTumblrToken (state, value) {
-      state.tumblr = value
+      state.tumblrToken = value
     }
   },
   getters: {
@@ -68,6 +68,6 @@ export default new Vuex.Store({
     updateLog (state) { return state.updateLog },
     savedLists(state) { return state.savedLists },
     savedProfiles(state) { return state.savedProfiles },
-    tumblrToken (state) { return state.tumblr }
+    tumblrToken (state) { return state.tumblrToken }
   }
 })
