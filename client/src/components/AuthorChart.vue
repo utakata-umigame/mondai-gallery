c3b67acae892f034d756deb8c6ff12f692cd54de
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-11-22 16:53:00 +0900

    Watch props update at charts

--- client/src/components/AuthorChart.vue
+++ client/src/components/AuthorChart.vue
@@ -6,29 +6,39 @@ export default {
   props: {
     list: Array
   },
+  methods: {
+    render () {
+      let counts = new Map()
+      this.list.forEach(m => {
+        let val = counts.get(m.author) || 0
+        counts.set(m.author, val+1)
+      })
+      let sorted = [...counts.entries()].sort((x, y) => y[1] - x[1])
+      let labels = sorted.map(x => x[0])
+      let data = sorted.map(x => x[1])
+      this.renderChart({
+        labels: labels,
+        datasets: [
+          {
+            backgroundColor: [
+              '#41B883',
+              '#E46651',
+              '#00D8FF',
+              '#DD1B16'
+            ],
+            data: data
+          }
+        ]
+      }, {responsive: true, maintainAspectRatio: false})
+    }
+  },
+  watch: {
+    list () {
+      this.render()
+    }
+  },
   mounted () {
-    let counts = new Map()
-    this.list.forEach(m => {
-      let val = counts.get(m.author) || 0
-      counts.set(m.author, val+1)
-    })
-    let sorted = [...counts.entries()].sort((x, y) => y[1] - x[1])
-    let labels = sorted.map(x => x[0])
-    let data = sorted.map(x => x[1])
-    this.renderChart({
-      labels: labels,
-      datasets: [
-        {
-          backgroundColor: [
-            '#41B883',
-            '#E46651',
-            '#00D8FF',
-            '#DD1B16'
-          ],
-          data: data
-        }
-      ]
-    }, {responsive: true, maintainAspectRatio: false})
+    this.render()
   }
 }
 </script>
--- client/src/components/SiteChart.vue
+++ client/src/components/SiteChart.vue
@@ -12,28 +12,38 @@ export default {
     }
   },
   mounted () {
-    let counts = new Map()
-    this.list.forEach(m => {
-      let val = counts.get(m.site) || 0
-      counts.set(m.site, val+1)
-    })
-    let sorted = [...counts.entries()].sort((x, y) => y[1] - x[1])
-    let labels = sorted.map(x => this.site[x[0]].name)
-    let data = sorted.map(x => x[1])
-    this.renderChart({
-      labels: labels,
-      datasets: [
-        {
-          backgroundColor: [
-            '#41B883',
-            '#E46651',
-            '#00D8FF',
-            '#DD1B16'
-          ],
-          data: data
-        }
-      ]
-    }, {responsive: true, maintainAspectRatio: false})
+    this.render()
+  },
+  watch: {
+    list () {
+      this.render()
+    }
+  },
+  methods: {
+    render () {
+      let counts = new Map()
+      this.list.forEach(m => {
+        let val = counts.get(m.site) || 0
+        counts.set(m.site, val+1)
+      })
+      let sorted = [...counts.entries()].sort((x, y) => y[1] - x[1])
+      let labels = sorted.map(x => this.site[x[0]].name)
+      let data = sorted.map(x => x[1])
+      this.renderChart({
+        labels: labels,
+        datasets: [
+          {
+            backgroundColor: [
+              '#41B883',
+              '#E46651',
+              '#00D8FF',
+              '#DD1B16'
+            ],
+            data: data
+          }
+        ]
+      }, {responsive: true, maintainAspectRatio: false})
+    }
   }
 }
 </script>
