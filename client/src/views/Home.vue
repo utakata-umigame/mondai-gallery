bfa961f24179ed052c72c8f9a25b47ad9f343be2
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-01-19 01:33:47 +0900

    Update Home.vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,5 @@
 <template>
   <div>
-    <nav class="breadcrumb" aria-label="breadcrumbs">
-      <ul>
-        <li class="is-active">
-          <router-link to="/home">
-            <span class="icon is-small is-primary">
-              <b-icon icon="home"/>
-            </span>
-            <span>ホーム</span>
-          </router-link>
-        </li>
-      </ul>
-    </nav>
     <div class="columns">
       <div class="column is-8 has-background-white">
         <b-tabs v-model="activeTab" position="is-centered" @change="changed" expanded>
